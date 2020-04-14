834ad2653869aa1176cafe0a49d5b6d9761279ba
PPranesh <ppranesh5@gmail.com>
2020-04-15 00:50:53 +0530

    version 1 completed

---
+++ date.js
@@ -0,0 +1,24 @@
+
+exports.getDate =  () => {
+        
+    const weekDay = new Date();
+
+    const options = {
+        month: "long",
+        day: "numeric"
+    };
+
+    return weekDay.toLocaleDateString("en-US",options);
+};
+
+exports.getDay = function () {
+        
+    const weekDay = new Date();
+
+    const options = {
+        weekday: "long"
+    };
+
+    return weekDay.toLocaleDateString("en-US",options);
+    
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+const todoItems = ["My Dream Job", "My MacBook Air", "My Cycle"];
+const worktodoItems = [];
+
+app.get("/", function(req, res){
+    
+    var day = date.getDay()+", "+date.getDate();
+    
+    res.render("lists", {itemTitle: day, newItem: todoItems});
+});
+
+app.post("/", function(req, res) {
+
+    let todo = req.body.todos;
+
+    if (req.body.listType === "Work") {
+        worktodoItems.push(todo);
+        res.redirect("/work");
+    } else {
+        todoItems.push(todo);
+        res.redirect("/");
+    }
+    
+});
+
+app.get("/work", function(req, res){
+    res.render("lists", {itemTitle: "Work To-Do", newItem: worktodoItems});
+});
+
+app.post("/work", function(req, res){
+    res.redirect("/work");
+});
+
+app.listen(3000, function(){
+    console.log("server started!!");
+});
